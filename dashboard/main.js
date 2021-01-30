914ddfc7c97fee5a58cd80ea3e59af1dd668813c
PsychoticPeach <miguel.gois2@gmail.com>
2021-01-30 18:06:58 +0000

    botão a 100%

--- dashboard/main.js
+++ dashboard/main.js
@@ -1,19 +1,3 @@
-const toggleColorMode = e => {
-    if (e.currentTarget.classList.contains("light--hidden")) {
-        document.documentElement.setAttribute("color-mode", "light");
-        localStorage.setItem("color-mode", "light");
-        return;
-    }
-    document.documentElement.setAttribute("color-mode", "dark");
-    localStorage.setItem("color-mode", "dark");
-};
-
-const toggleColorButtons = document.querySelectorAll(".color-mode__btn");
-
-toggleColorButtons.forEach(btn => {
-    btn.addEventListener("click", toggleColorMode);
-})
-
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
 function switchTheme(e) {
